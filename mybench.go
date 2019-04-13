9ad8496e492210fd917f016c463fc81ea5ad47bf
hiepndd <nguyendangdaihiep@gmail.com>
2019-04-14 01:55:51 +0700

    implement required function

--- mybench.go
+++ mybench.go
@@ -3,7 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
+
+type responseInfo struct {
+	status   int
+	bytes    int64
+	duration time.Duration
+}
+
+type summaryInfo struct {
+	requested int64
+	responded int64
+}
+
+func checkLink(link string, c chan responseInfo) {
+	start := time.Now()
+	res, err := http.Get(link)
+	if err != nil {
+		panic(err)
+	}
+	read, _ := io.Copy(ioutil.Discard, res.Body)
+	c <- responseInfo{
+		status:   res.StatusCode,
+		bytes:    read,
+		duration: time.Now().Sub(start),
+	}
+}
 
 func main() {
 	fmt.Println("Hello from my app")
@@ -11,5 +41,31 @@ func main() {
 	concurrency := flag.Int64("c", 1, "number of requests to perform at once")
 	fmt.Println(request, concurrency)
 	flag.Parse()
-	flag.PrintDefaults()
+
+	if flag.NArg() == 0 || *request == 0 || *request < *concurrency {
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
+
+	link := flag.Arg(0)
+	c := make(chan responseInfo)
+	summary := summaryInfo{}
+
+	for i := int64(0); i < *concurrency; i++ {
+		summary.requested++
+		go checkLink(link, c)
+	}
+
+	for response := range c {
+		if summary.requested < *request {
+			summary.requested++
+			go checkLink(link, c)
+		}
+		summary.responded++
+		fmt.Println(response)
+		if summary.responded == summary.requested {
+			break
+		}
+	}
+
 }
